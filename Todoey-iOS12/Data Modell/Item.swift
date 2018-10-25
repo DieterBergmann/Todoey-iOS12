b442616850979ef7ce3acb95d0027da6d128f3d5
Dieter Bergmann <dieterbergmann@arcor.de>
2018-10-25 06:27:27 +0200

    Convert to MVC design pattern.

---
+++ Todoey-iOS12/Data Modell/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey-iOS12
+//
+//  Created by Dieter Bergmann on 24.10.18.
+//  Copyright © 2018 Dieter Bergmann. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
